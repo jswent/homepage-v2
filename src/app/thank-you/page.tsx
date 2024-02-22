b9605e6bd063edada049a7930c42ebcd1f407dcb
James Swent <jake@osprey21.com>
2024-02-22 22:33:09 +0000

    (feat): Specify ThankYou query params

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,17 +1,28 @@
 import { type Metadata } from 'next'
 
 import { SimpleLayout } from '@/components/SimpleLayout'
+import NotFound from '../not-found'
 
 export const metadata: Metadata = {
   title: 'You’re subscribed',
   description: 'Thanks for subscribing to my newsletter.',
 }
 
-export default function ThankYou() {
-  return (
-    <SimpleLayout
-      title="Thanks for subscribing."
-      intro="I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings."
-    />
-  )
+export default function ThankYou({
+  params,
+  searchParams,
+}: {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  if (searchParams.status && searchParams.status == 'subscription-confirmed') {
+    return (
+      <SimpleLayout
+        title="Thanks for subscribing."
+        intro="I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings."
+      />
+    )
+  }
+
+  return NotFound()
 }
